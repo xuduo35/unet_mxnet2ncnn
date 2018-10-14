6cbaa8d1011327975e1ae9b2a37b3c36426a7d1b
xuduo35 <xiexiecn@hotmail.com>
2018-10-14 23:27:18 +0800

    add check.py for debug ncnn model

---
+++ mxnet-unet/check.py
@@ -0,0 +1,98 @@
+import os
+os.environ["MXNET_BACKWARD_DO_MIRROR"] = "1"
+os.environ["MXNET_CUDNN_AUTOTUNE_DEFAULT"] = "0"
+import sys
+import cv2
+import mxnet as mx
+from mxnet import ndarray as F
+from skimage.transform import resize
+from skimage.io import imsave
+import numpy as np
+from unetdataiter import UnetDataIter
+import matplotlib.pyplot as plt
+from unet import build_unet
+
+np.set_printoptions(threshold=np.inf)
+
+def post_process_mask(label, img_cols, img_rows, n_classes, p=0.5):
+    pr = label.reshape(n_classes, img_cols, img_rows).transpose([1,2,0]).argmax(axis=2)
+    return (pr*255).asnumpy()
+
+def ncnn_output(label):
+    #pr = label.reshape(channels, img_cols, img_rows).transpose([1,2,0])
+    pr = label.transpose([1,2,0])
+    return pr.asnumpy()
+
+
+def load_image(img, width, height):
+    im = np.zeros((height, width, 3), dtype='uint8')
+    #im[:, :, :] = 128
+
+    if img.shape[0] >= img.shape[1]:
+        scale = img.shape[0] / height
+        new_width = int(img.shape[1] / scale)
+        diff = (width - new_width) // 2
+        img = cv2.resize(img, (new_width, height))
+
+        im[:, diff:diff + new_width, :] = img
+    else:
+        scale = img.shape[1] / width
+        new_height = int(img.shape[0] / scale)
+        diff = (height - new_height) // 2
+
+        img = cv2.resize(img, (width, new_height))
+        im[diff:diff + new_height, :, :] = img
+
+    im = np.float32(im) / 255.0
+
+    return [im.transpose((2,0,1))]
+
+def main():
+    batch_size = 16
+    n_classes = 2
+    img_width = 256
+    img_height = 256
+    #img_width = 96
+    #img_height = 96
+
+    ctx = [mx.gpu(0)]
+
+    # sym, arg_params, aux_params = mx.model.load_checkpoint('unet_person_segmentation', 20)
+    #unet_sym = build_unet(batch_size, img_width, img_height, False)
+    # unet = mx.mod.Module(symbol=unet_sym, context=ctx, label_names=None)
+
+    sym, arg_params, aux_params = mx.model.load_checkpoint('unet_person_segmentation', 0)
+    all_layers = sym.get_internals()
+    print(all_layers.list_outputs())
+    unet = mx.mod.Module(symbol=all_layers['conv11_1_output'], context=ctx, label_names=None)
+    #unet = mx.mod.Module(symbol=all_layers['pool5_output'], context=ctx, label_names=None)
+
+    unet.bind(for_training=False, data_shapes=[['data', (batch_size, 3, img_width, img_height)]], label_shapes=unet._label_shapes)
+    #unet.set_params(arg_params, aux_params, allow_missing=True)
+    unet.set_params(arg_params, aux_params)
+
+    testimg = cv2.imread(sys.argv[1], 1)
+    img = load_image(testimg, img_width, img_height)
+    unet.predict(mx.io.NDArrayIter(data=[img]))
+
+    outputs = unet.get_outputs()[0]
+    print(outputs[0].shape)
+    output = ncnn_output(outputs[0])
+    print(output) 
+
+    #keys = unet.get_params()[0].keys() # 列出所有权重名称
+    #print(keys)
+    #conv_w = unet.get_params()[0]['trans_conv6_weight'] # 获取想要查看的权重信息
+    #print(conv_w.shape)
+    #print(conv_w.asnumpy()) # 查看具体数值
+
+    #cv2.imshow('test', testimg)
+    #cv2.imshow('mask', post_process_mask(outputs[0], img_width, img_height, n_classes))
+    #cv2.waitKey()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("illegal parameters")
+        sys.exit(0)
+
+    main()
