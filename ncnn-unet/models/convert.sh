9f51dea189c63805cbd0a0d5b957dca8e5050910
xuduo35 <xiexiecn@hotmail.com>
2018-10-15 09:39:52 +0800

    invalid path

--- ncnn-unet/models/convert.sh
+++ ncnn-unet/models/convert.sh
@@ -1,2 +1,2 @@
 rm -f ncnn.*
-../../ncnn/build/tools/mxnet/mxnet2ncnn /disk/223/xiejb231/unet_mxnet2ncnn/mxnet-unet/unet_person_segmentation-symbol.json /disk/223/xiejb231/unet_mxnet2ncnn/mxnet-unet/unet_person_segmentation-0000.params
+../../../ncnn/build/tools/mxnet/mxnet2ncnn /disk/223/xiejb231/unet_mxnet2ncnn/mxnet-unet/unet_person_segmentation-symbol.json /disk/223/xiejb231/unet_mxnet2ncnn/mxnet-unet/unet_person_segmentation-0000.params
